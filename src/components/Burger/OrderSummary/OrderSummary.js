45db34d1c40b95294b21eaa2003bf8ff0a149e13
Mihai Robu <mihairobu@Mihais-MacBook-Air.local>
2020-06-24 12:00:55 -0600

    Lesson 12.222 added checkout container

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -29,8 +29,8 @@ class OrderSummary extends Component {
                 </ul>
                 <p><strong>Total Price: {this.props.price.toFixed(2)} </strong></p>
                 <p>Continue to Checkout?</p>
-                <Button btnType='Success' clicked={this.props.purchaseCancelled}>CANCEL</Button>
-                <Button btnType='Danger' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+                <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
             </Aux>
         );
     }
