68e73b2ddd1d182ef31076471b075ae6a2c299f1
Mihai Robu <mihairobu@Mihais-MacBook-Air.local>
2020-06-24 13:06:10 -0600

    Lessons 12.223-225 setting up routes and navigating forward/back with buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,28 +93,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Mi Hello',
-                address: {
-                    street: 'Teststreet',
-                    zipCode: '415151',
-                    country: 'Germany',
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false })
-            });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Mi Hello',
+        //         address: {
+        //             street: 'Teststreet',
+        //             zipCode: '415151',
+        //             country: 'Germany',
+        //         },
+        //         email: 'test@test.com',
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false });
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false })
+        //     });
+        this.props.history.push('/checkout')
     }
 
     render() {
