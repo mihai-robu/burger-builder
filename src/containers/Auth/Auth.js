a5cfefe59af3434f6cfd61c8f1841fcf88fc8071
Mihai Robu <mihairobu@Mihais-MacBook-Air.local>
2020-07-04 18:40:32 -0600

    Lesson 18.327-328 getting token from backend and adding sign-in

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
+import { connect } from 'react-redux';
 
 class Auth extends Component {
     state = {
@@ -34,6 +36,7 @@ class Auth extends Component {
                 valid: false,
                 touched: false,
             },
+            isSignup: true,
         }
     }
 
@@ -69,7 +72,18 @@ class Auth extends Component {
                 touched: true
             }
         }
-        this.setState({controls: updatedControls});
+        this.setState({ controls: updatedControls });
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return { isSignup: !prevState.isSignup };
+        })
     }
 
     render() {
@@ -95,13 +109,22 @@ class Auth extends Component {
         ));
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType='Success'>SUBMIT</Button>
                 </form>
+                <Button 
+                    clicked={this.switchAuthModeHandler}
+                    btnType="Danger">SWITCH TO {this.state.isSignup ? 'SIGN IN' : "SIGN UP"}</Button>
             </div>
         );
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
---
+++ src/store/actions/auth.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData,
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDc9M5O4RhqP5jk4vlP-2jGEei9HAstVII';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDc9M5O4RhqP5jk4vlP-2jGEei9HAstVII'
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch((authSuccess(response.data)));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
+    };
+};
