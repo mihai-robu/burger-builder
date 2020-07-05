8a141459622fffb1db318aec51c23a97e183a2f7
Mihai Robu <mihairobu@Mihais-MacBook-Air.local>
2020-07-05 16:55:44 -0600

    Lesson 18.333-334 changing UI based on auth state and adding logout link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Route path='/checkout' component={Checkout} />
             <Route path='/orders' exact component={Orders} />
             <Route path='/auth' exact component={Auth} />
+            <Route path='/logout' exact component={Logout} />
             <Route path='/' exact component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/' exact>Burger Builder</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
-        <NavigationItem link='/auth'>Log In</NavigationItem>
+        {!props.isAuthenticated
+            ? <NavigationItem link='/auth'>Log In</NavigationItem>
+            : <NavigationItem link='/logout'>Log Out</NavigationItem>}
     </ul>
 )
 
