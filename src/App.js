8b48dee03a9c15a02054186e72761ce5868e26ae
Mihai Robu <mihairobu@Mihais-MacBook-Air.local>
2020-07-05 18:06:48 -0600

    Lesson 18.337-338 using localStorage to stay signed in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -6,10 +6,18 @@ import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  };
+
+  render() {
+    return (
+      <div>
         <Layout>
           <Switch>
             <Route path='/checkout' component={Checkout} />
@@ -19,8 +27,15 @@ function App() {
             <Route path='/' exact component={BurgerBuilder} />
           </Switch>
         </Layout>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+  const mapDispatchToProps = dispatch => {
+    return {
+      onTryAutoSignup: () => dispatch(actions.authCheckState()),
+    };
+  };
+
+  export default connect(null, mapDispatchToProps)(App);
